e00969daec7c995a54a14bef93843b822b763d06
EdanAlmeida <edluisalmeida@gmail.com>
2023-09-17 23:07:08 -0300

    Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,26 @@ import { Component, OnInit } from '@angular/core';
       <h3>Acaba aqui o NgContent</h3>
     </app-diretivas-atributos>-->
     <!--<app-diretivas-atributos>-->
-    <app-new-component>
+    <!--<app-new-component></app-new-component>-->
+    <app-input [contador]="addValue"></app-input>
+    <br>
+    <button (click)="add()">Add</button>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
   
+  public addValue: number = 0;
   constructor(){}
 
   ngOnInit(): void {
     setTimeout( ()=> {
       console.log(1);
     }, 5000);
+  }
+
+  public add() {
+    this.addValue += 1;
   }
 
 }
